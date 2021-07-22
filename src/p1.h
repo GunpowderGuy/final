9f5fb7e5a8c3087cd0ca6c745ee9d82861e71c85
GunpowderGuy <diegorosario2013@gmail.com>
2021-07-21 21:41:41 -0500

    entrega dia examen

--- src/p1.h
+++ src/p1.h
@@ -5,6 +5,33 @@
 #ifndef POO2_PC2_SEC01_V2021_1_P1_H
 #define POO2_PC2_SEC01_V2021_1_P1_H
 
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void eliminar(double elemento, vector<vector<double>> &cont) {
+  auto backup = cont;
+  auto subcont1 = cont[0];
+  auto puntero = find(subcont1.begin(), subcont1.end(), elemento);
+
+  while (*puntero != subcont1.back()) {
+    for (auto subcont : cont) {
+      puntero = find(subcont.begin(), subcont.end(), elemento);
+    }
+  }
+}
+
+pair<double, double> minimum_range(const vector<vector<double>> &containers) {
+
+  std::priority_queue<double, std::vector<double>, std::greater<double>>
+      min_heap;
+
+  //for ()
+}
+
 void question_1_1();
 
-#endif //POO2_PC2_SEC01_V2021_1_P1_H
+#endif // POO2_PC2_SEC01_V2021_1_P1_H
--- src/p2.h
+++ src/p2.h
@@ -5,6 +5,31 @@
 #ifndef POO2_PC2_SEC01_V2021_1_P2_H
 #define POO2_PC2_SEC01_V2021_1_P2_H
 
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+inline vector<double> rankear(const vector<double> &vec) {
+  vector<double> temp = vec;
+  sort(temp.begin(), temp.begin());
+  unique(temp.begin(), temp.end());
+
+  vector<double> result = vec;
+
+  for (double elem : result) {
+    elem = *find(temp.begin(), temp.end(), elem);
+  }
+
+  return result;
+}
+
+template <typename Cont> auto rank_values(Cont cont) {
+  vector<double> temp(cont.begin(), cont.end());
+
+  return rankear(temp);
+}
+
 void question_2_1();
 
-#endif //POO2_PC2_SEC01_V2021_1_P2_H
+#endif // POO2_PC2_SEC01_V2021_1_P2_H
