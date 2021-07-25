3cd83fadae658b709e5760550491e380b1eb6800
GunpowderGuy <diegorosario2013@gmail.com>
2021-07-25 15:13:42 -0500

    exercise 2 seems to be working

--- src/p2.h
+++ src/p2.h
@@ -5,38 +5,40 @@
 #ifndef POO2_PC2_SEC01_V2021_1_P2_H
 #define POO2_PC2_SEC01_V2021_1_P2_H
 
-template<typename T, template <typename...> class CType>
+template <typename T, template <typename...> class CType>
 CType<T> rank_values(CType<T> cnt) {
-    return CType<T>{};
+  return CType<T>{};
 }
 
 void question_2_1();
 
-
 #include <algorithm>
+#include <iterator>
 #include <vector>
 
 using namespace std;
 
-inline vector<double> rankear(const vector<double> &vec) {
-  vector<double> temp = vec;
-  sort(temp.begin(), temp.begin());
+template <typename T> inline vector<T> rank_values(const vector<T> &vec) {
+
+  vector<T> temp = vec;
+  sort(temp.begin(), temp.end());
   unique(temp.begin(), temp.end());
 
-  vector<double> result = vec;
+  vector<T> result = vec;
 
-  for (double elem : result) {
-    elem = *find(temp.begin(), temp.end(), elem);
+  for (T &elem : result) {
+    elem = distance(temp.begin(), find(temp.begin(), temp.end(), elem)) + 1;
   }
 
   return result;
 }
 
+/*
 template <typename Cont> auto rank_values(Cont cont) {
   vector<double> temp(cont.begin(), cont.end());
 
   return rankear(temp);
 }
+*/
 
-
-#endif //POO2_PC2_SEC01_V2021_1_P2_H
+#endif // POO2_PC2_SEC01_V2021_1_P2_H
